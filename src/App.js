a76c90feb363e2955d6370822fc7b60718ea5e5b
lyndabelfar <lyndabelar@gmail.com>
2022-09-16 23:38:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import dayImg from './images/bg-image-daytime.jpg'
+import nightImg from './images/bg-image-nighttime.jpg'
+
+
 
 function App() {
+  const [quotes, SetQuotes] = useState([])
+  const [num, SetNum] = useState(0)
+
+
+
+
+
+  let currentTime = new Date();
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+
+  
+  let time = hours +':'+ minutes;
+
+  const url= "https://type.fit/api/quotes";
+
+
+  const fetchData = async ()=>{
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    SetQuotes(data)
+    
+  }
+  const handleRefreshBtn =()=>{
+    let newNum = num +1; 
+    SetNum(newNum)
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[num])
+  const randomQuoteIndex = Math.round(Math.random()*quotes.length);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={4>hours>18?dayImg: nightImg} alt="bg-img" className="bg-img" />
+      <div className="container">
+        <div className="quote-div">
+          <div className="text">
+            <div className="quote">{quotes[randomQuoteIndex].text}</div>
+            <div className="author">{quotes[randomQuoteIndex].author}</div>
+          </div>
+          <button className="refresh-btn" onClick={handleRefreshBtn}>Refresh</button>
+        </div>
+
+        <div className="greetings">
+          {
+            4>hours>18?'Good Morning! It is currently':'Good Evening! It is currently'
+          }
+        </div>
+        <h1 className="time">{time}</h1> 
+      </div>
+      
+    </>
   );
 }
 
